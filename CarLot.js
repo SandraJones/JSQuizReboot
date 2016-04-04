0b1ae4c2388d348b4c58e824e783db074f98a6d8
Sandy Jones <sandynashv@gmail.com>
2016-04-03 19:19:18 -0500

    function rehab

--- CarLot.js
+++ CarLot.js
@@ -1,32 +1,24 @@
 "use strict";
 var CarLot = (function () {
-	console.log("CarLot");
   var privateInventory = [];
 
   return {
-    // getInventory: function () {
-    //   return privateInventory;
-    //    console.log("inventory");
-    // }, 
-    loadInventory: function (callback) {
-    	console.log("loadInventory function");
+    getInventory: function () {
+      return privateInventory;
+       console.log("inventory");
+    }, 
+    loadInventory: function(callbackFunction) {
       var invLoader = new XMLHttpRequest();
-
       invLoader.addEventListener("load", function () {
-			console.log("listener");
-			//set the value of the private array
-			  privateInventory = JSON.parse(this.responseText);
-			  console.log("privateInventory", privateInventory);
-      });
-      invLoader.open("GET", "inventory.json");
-      invLoader.send();
-    	console.log(this);
-    	// callback();
+				//set the value of the private array
+				  privateInventory = JSON.parse(this.responseText).inventory;
+      	  
+      	  callbackFunction(privateInventory);
+      	});
+    	
+	      invLoader.open("GET", "inventory.json");
+	      invLoader.send();
    }
   } 
-  populate(inventory);
-  //list cars in the DOM
-      // var list = document.getElementByClassName("output");
-      // return 
 })();
-CarLot.loadInventory();
+// CarLot.loadInventory();
--- quiz.js
+++ quiz.js
@@ -1,19 +1,24 @@
 "use strict";
-var populate = function (inventory) {
-  // Loop over the inventory and populate the page
-  
-  let outputEl = document.getElementsByClassName("output")
-    outputEl.innerHTML = "";
-  for (var i = 0; i < CarLot.inventory.length; i++) {
-  	 let inventoryCars = CarLot.getInventory();
-  	 ouptutEl.innerHTML += `<div class="output" class="container"> + inventory[i].cars +
-		</div>`;
-  }
-  // Now that the DOM is loaded, establish all 
-  // the event listeners needed
-}
-// Load the inventory and send a callback 
-// function to be invoked after the process is complete
-
-CarLot.loadInventory(populate);
-
+var CarLot = (function(inventory) {
+		function populate(inventory) {
+		  // Loop over the inventory and populate the page
+		  var list = document.getElementsByClassName("output");
+		  var outputString = "";
+		  for (var i = 0; i < CarLot.inventory.length; i++) {
+		  	 var currentCar = CarLot.getInventory();
+		  	 //build up DOM string
+		  	 outputString += `<div class="row">
+          <div class="col-xs-4"><h1>${currentInventory.make}</h1>
+  	<h2>${currentInventory.model}</h2><h3>${currentInventory.year}</h3>${currentInventory.price}</h3><h4>${currentInventory.color}</h4>
+  	<h5>${currentInventory.purchased}</h5><h6>${currentInventory.description}</h6></div>;
+          </div>`;
+          // <div class="output" class="container"> + inventory[i].cars + </div>`;
+		  list.innerHTML += outputString; 
+		  }
+		  // Now that the DOM is loaded, establish all 
+		  // the event listeners needed
+		}
+		// Load the inventory and send a callback 
+		// function to be invoked after the process is complete
+   return inventory;
+})(CarLot);
