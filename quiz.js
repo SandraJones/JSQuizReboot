cab06fe66a3ab8da81954f8a273dc3eba96e5cb7
Sandy Jones <sandynashv@gmail.com>
2016-04-07 15:01:04 -0500

    added for loop

--- quiz.js
+++ quiz.js
@@ -1,20 +1,39 @@
 "use strict";
-
 let privateInventory = [];  	
      //build up DOM string
 function buildCarDisplay() {
   for (let i = 0; i < privateInventory.length; i++){
 		let container = "";
-    container[i].innerHTML += `<div class="output1 container row"><section class="col-xs-4 border">${privateInventory.make}${privateInventory.model}${privateInventory.year}${privateInventory.price}${privateInventory.color}${privateInventory.description}</section>    
+    container[i].innerHTML += `<div class="output1 container row"><section class="col-xs-4 border">${privateInventory[i].make}${privateInventory[i].model}${privateInventory[i].year}${privateInventory[i].price}${privateInventory[i].color}${privateInventory[i].description}</section>    
     </div>`;
   }
 }   
+function removeSelected() {
+  for (let i=0; i< privateInventory; i++) {
+    privateInventory[i].classList.remove("selected");
+  }
+}
+
+function keyEvent(currentCard, currentBio){
+  input.addEventListener("keyup", function(event) {
+    //if returns a boolean, so check if currentCard
+    if (currentCar.classList.contains("selected")) {
+      let newDescription = event.currentTarget.value;
+      currentDescription.innerHTML = newDescription; 
+      if (event.keyCode === 13) {
+         currentDescription.innerHTML = newDescription;
+         input.value = "";
+      }   
+    }
+  });
+}
+
   // Now that the DOM is loaded, establish all 
   // the event listeners needed
 function addClickEvent(privateInventory) {
   for (let i=0; i < privateInventory; i++){
-    let currentCar = privateInventory.car[i].make.model;
-    let currentDescription = privateInventory.car[i].description;
+    let currentCar = privateInventory[i].cars.make;
+    let currentDescription = privateInventory[i].cars.description;
     currentCar.addEventListener("click", function(event) {
       removeSelected();
       input.value =  "";
@@ -26,12 +45,10 @@ function addClickEvent(privateInventory) {
 };
 // Load the inventory and send a callback 
 // function to be invoked after the process is complete
-
 function populate(privateInventory) {
   console.log("privateInventory", privateInventory );
   // Loop over the inventory and populate the page
-  let list = document.getElementsByClassName("output1");
-  
+  let list = document.getElementsByClassName("output1");  
   for (let i = 0; i < privateInventory.length; i++) {
     let currentCar = privateInventory[i];
     buildCarDisplay(privateInventory);
