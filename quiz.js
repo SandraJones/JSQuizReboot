4638ab27b0ec7d744c312768e1e552254ff9451b
Sandy Jones <sandynashv@gmail.com>
2016-04-07 13:04:20 -0500

    compress classes

--- quiz.js
+++ quiz.js
@@ -1,12 +1,16 @@
-var privateInventory = [];  	
+"use strict";
+
+let privateInventory = [];  	
      //build up DOM string
 function buildCarDisplay() {
-		container.innerHTML += `<div class="output1 container row"><section class="col-xs-4" class = "border">${privateInventory.make}${privateInventory.model}${privateInventory.year}${privateInventory.price}${privateInventory.color}${privateInventory.description}</section>    
+  for (let i = 0; i < privateInventory.length; i++){
+		let container = "";
+    container[i].innerHTML += `<div class="output1 container row"><section class="col-xs-4 border">${privateInventory.make}${privateInventory.model}${privateInventory.year}${privateInventory.price}${privateInventory.color}${privateInventory.description}</section>    
     </div>`;
-   }   
+  }
+}   
   // Now that the DOM is loaded, establish all 
   // the event listeners needed
-
 function addClickEvent(privateInventory) {
   for (let i=0; i < privateInventory; i++){
     let currentCar = privateInventory.car[i].make.model;
@@ -26,10 +30,10 @@ function addClickEvent(privateInventory) {
 function populate(privateInventory) {
   console.log("privateInventory", privateInventory );
   // Loop over the inventory and populate the page
-  var list = document.getElementsByClassName("output1");
+  let list = document.getElementsByClassName("output1");
   
-  for (var i = 0; i < privateInventory.length; i++) {
-    var currentCar = privateInventory[i];
+  for (let i = 0; i < privateInventory.length; i++) {
+    let currentCar = privateInventory[i];
     buildCarDisplay(privateInventory);
   }   
   addClickEvent();
