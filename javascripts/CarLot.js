7f24f72276eae7b4622be8dfd6e630d2747db23e
Sandy Jones <sandynashv@gmail.com>
2016-04-09 15:46:00 -0500

    new javascripts file

---
+++ javascripts/CarLot.js
@@ -0,0 +1,21 @@
+var CarLot = (function(){
+  var privateInventory = [];
+
+  return {
+    // getInventory: function () {
+    //   return privateInventory;
+    //    console.log("inventory");
+    // }, 
+    loadInventory: function (callBack) {
+      var invLoader = new XMLHttpRequest();
+	      invLoader.open("GET", "inventory.json");
+	      invLoader.send();
+        invLoader.addEventListener("load", function () {
+        //set the value of the private array
+          privateInventory = JSON.parse(this.responseText).cars;
+          console.log("privateInventory", privateInventory);
+        callBack(privateInventory);
+        });
+    }
+  }; 
+})();
