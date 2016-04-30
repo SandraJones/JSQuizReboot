2f50b3ea585b706328fc3565a580aa69debcbb90
Sandy Jones <sandynashv@gmail.com>
2016-04-30 17:24:51 -0500

    added listeners for thicker border

--- eventHandler.js
+++ eventHandler.js
@@ -1,19 +1,70 @@
 "use strict";
 var CarLot = (function(oldCarLot) {
 	var privateInventory = CarLot.getInventory();
-	//above works
-	 oldCarLot.changeBorder = function(color1) {
-	 	console.log("changeBorderfunction");
-	    document.getElementById("outputTarget").style.border = "thin solid red";   		
+	var card = document.getElementsByClassName("card");   
+	var classId = "";
+	var clickedThings = document.getElementsByClassName("selected")
+	var input = document.getElementById("inputSpot");
+
+	oldCarLot.addClickEvent = function() {
+		for (var i=0; i<card.length; i++) {
+				 var currentClass =  card[i];
+				 currentClass.addEventListener("click", CarLot.markClicked);
 		};
-	  oldCarLot.changeColor = function() {
-	  	console.log("changeColorfunction");
-	  	document.getElementById("outputTarget").style.background = "grey";
-	    border.addEventListener("click", function(event)  {
-	    });  
-		}
-		oldCarLot.editDescription = function() {
-			console.log("changeDescription");
-		}	
+	};
+
+	oldCarLot.removeStyling = function(){
+		console.log("2	removeStyling fired");
+		for (var i=0; i<card.length; i++){
+			console.log("3	card i ", card[i]);
+			//removes the class of the card that was selected previously
+			card[i].classList.remove("fatBorder");
+			console.log("4 card i ", card[i]);
+
+		};
+	};
+
+	oldCarLot.markClicked = function(){
+		console.log("1 markClicked");
+		oldCarLot.removeStyling();
+		input.value =  "";
+		console.log("5", input.value);
+    input.focus();
+		var clickedClass = event.target.closest('section');
+		console.log("6 clickedClass", clickedClass);
+		classId = clickedClass.getAttribute("id");
+		console.log("7 classId", classId);
+
+		if (clickedThings.length !== 0) {
+				clickedClass.classList.remove("selected");
+				console.log("	8 clicked", clickedClass[0]);
+				clickedClass.classList.add("selected");
+			}
+		else {
+				clickedClass.classList.add("selected");
+			}
+		oldCarLot.chgBorderEvent(clickedClass);	
+    oldCarLot.editor(clickedClass);
+	};
+
+
+	oldCarLot.editor = function(classId){
+	  let currentDescr = classId.lastElementChild;
+	  	console.log(currentDescr);	
+	  inputSpot.addEventListener("keyup", function() {
+  	let newDescr = inputSpot.value;
+  		console.log("	new", newDescr);
+		  currentDescr.innerHTML = newDescr;
+		    if (event.keyCode === 13) {
+		       input.value = "";
+	    	};     	
+	 	 });
+	};
+
+	oldCarLot.chgBorderEvent = function(classId) {
+		classId.classList.add("fatBorder");
+	};	
+	
 	return oldCarLot;
+
 })(CarLot);
